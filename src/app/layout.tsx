29c513c931214cf3c3b9724f2b0f53dfcc241a49
hunter <n.ohotnikov@collabox.dev>
2023-10-10 17:53:50 +0300

    created basic template

---
+++ src/app/layout.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Link from 'next/link';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import HomeIcon from '@mui/icons-material/Home';
+import StarIcon from '@mui/icons-material/Star';
+import ChecklistIcon from '@mui/icons-material/Checklist';
+import SettingsIcon from '@mui/icons-material/Settings';
+import SupportIcon from '@mui/icons-material/Support';
+import LogoutIcon from '@mui/icons-material/Logout';
+import ThemeRegistry from '@/shared/config/theme/ThemeRegistry';
+import Header from '@/shared/ui/common/Header';
+import Footer from '@/shared/ui/common/Footer';
+
+export const metadata = {
+  title: 'Next.js App Router + Material UI v5',
+  description: 'Next.js App Router + Material UI v5',
+};
+
+const DRAWER_WIDTH = 240;
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeRegistry>
+          <Header/>
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              bgcolor: 'background.default',
+              ml: `${DRAWER_WIDTH}px`,
+              mt: ['48px', '56px', '64px'],
+              p: 3,
+            }}
+          >
+            {children}
+          </Box>
+          <Footer/>
+        </ThemeRegistry>
+      </body>
+    </html>
+  );
+}
---
+++ src/shared/ui/common/Header.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+const Header = () => {
+  return (
+    <Box>Header</Box>
+  )
+}
+
+export default Header
