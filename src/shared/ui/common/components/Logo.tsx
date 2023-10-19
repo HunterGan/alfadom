127582edf7c31daf3ed9b7ca603549cd52d0c166
hunter <n.ohotnikov@collabox.dev>
2023-10-19 17:16:59 +0300

    view-1: added header, made shared common components

---
+++ src/shared/ui/common/components/Logo.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material"
+
+const Logo = () => {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        minHeight: 80,
+        background: '#fff',
+      }}
+    >
+      Alfa-Dom
+    </Box>
+  )
+}
+
+export default Logo
--- src/shared/ui/common/layouts/Footer.tsx
+++ src/shared/ui/common/layouts/Footer.tsx
@@ -1,6 +1,14 @@
-import { Box, Button, Divider, Grid, List, ListItem, ListItemText, Typography } from "@mui/material"
-import Content from "@/shared/ui/common/Content"
-import { FC } from "react"
+import {
+  Box,
+  Grid,
+  Button,
+  Divider
+} from "@mui/material"
+import Logo from '@/shared/ui/common/components/Logo'
+import Content from "@/shared/ui/common/layouts/Content"
+import Address from '@/shared/ui/common/components/Address'
+import LinksBox from '@/shared/ui/common/components/LinksBox'
+import WorkingAt from '@/shared/ui/common/components/WorkingAt'
 
 const darkTheme = {
   background: '#1F1F1F',
@@ -83,104 +91,6 @@ const Footer = () => {
           </Grid>
         </Grid>
       </Content>
-    </Box>
-  )
-}
-
-interface ILinksBox {
-  title: string
-  links: {
-    [key: string]: string
-  }
-}
-
-const LinksBox:FC<ILinksBox> = ({title, links}) => {
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        color: 'white',
-        fontSize: '14px',
-        fontWeight: 400,
-        opacity: 0.75,
-        '&>:first-child': {
-          fontWeight: 600,
-          opacity: 1,
-          fontSize: 17,
-        }
-      }}
-  >
-    <Typography>{title}</Typography>
-    <List>
-      {Object.entries(links).map(([key, value]) => (
-        <ListItem key={key} disablePadding>
-          <ListItemText>{value}</ListItemText>
-        </ListItem>
-      ))}
-    </List>
-  </Box>
-  )
-}
-
-const WorkingAt = () => {
-  return (
-    <Box>
-      <Typography sx={{fontSize: '18px',color: '#FFF'}}>Working at:</Typography>
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center'
-        }}
-      >
-        <Icon/>
-        <Typography sx={{fontSize: '18px',color: '#FFF'}}>
-          From
-          <Typography sx={{color: theme.textAccent, display: 'inline'}}> morning </Typography>
-          to
-          <Typography sx={{color: theme.textAccent, display: 'inline'}}> evening</Typography>
-        </Typography>
-      </Box>
-    </Box>
-  )
-}
-
-const Icon = () => (
-  <Box
-    sx={{
-      height: 18,
-      width: 18,
-      marginRight: '8px'
-    }}
-  >
-    <svg version="1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-      viewBox="0 0 24 24" enableBackground="new 0 0 24 24" xmlSpace="preserve">
-      <path fillRule="evenodd" clipRule="evenodd" fill={theme.svgClock} d="M12,0C5.4,0,0,5.4,0,12s5.4,12,12,12c6.6,0,12-5.4,12-12
-      S18.7,0,12,0z M16.4,17.9l-5.5-5.5l0,0h0V4.5h2.2v7l4.9,4.9L16.4,17.9z"/>
-    </svg>
-  </Box>
-)
-
-
-const Address = () => {
-  return (
-    <Box>
-      <Typography sx={{fontSize: '18px',color: '#FFF'}}>Address</Typography>
-      <Typography sx={{fontSize: '18px',color: '#FFF'}}>This is our address</Typography>
-    </Box>
-  )
-}
-
-const Logo = () => {
-  return (
-    <Box
-      sx={{
-        width: 200,
-        height: 80,
-        background: '#fff',
-      }}
-    >
-      Alfa-Dom
     </Box>
   )
 }
