e137fb4982077875283f331e45e6c1487bd6bdc9
hunter <n.ohotnikov@collabox.dev>
2023-10-25 18:06:34 +0300

    view-2: started creating phone form

--- src/pages_/home/ui/views/FirstScreen.tsx
+++ src/pages_/home/ui/views/FirstScreen.tsx
@@ -1,5 +1,7 @@
 import Content from "@/shared/ui/common/layouts/Content"
-import { Box, Grid, Typography } from "@mui/material"
+import { Box, Grid, InputAdornment, TextField, Typography } from "@mui/material"
+import Icon from '@/../public/assets/svg/phone_icon.svg'
+import Image from "next/image"
 
 const FirstScreen = () => {
   return (
@@ -22,7 +24,7 @@ const FirstScreen = () => {
           spacing={2}
         >
           <Grid
-            item xs={12} sm={6}
+            item xs={12} md={6}
             sx={{
               display: 'flex',
               alignItems: 'center',
@@ -44,7 +46,7 @@ const FirstScreen = () => {
             </Box>
           </Grid>
           <Grid
-            item xs={12} sm={6}
+            item xs={12} md={6}
             sx={{
               display: 'flex',
               alignItems: 'center',
@@ -53,10 +55,72 @@ const FirstScreen = () => {
           >
             <Box
               sx={{
-                maxWidth: 
+                width: 450,
+                backgroundColor: '#eaeaea',
+                opacity: 0.9,
+                margin: '20px',
+                padding: '20px',
+                backdropFilter: 'blur(10px)',
+                borderRadius: '6px',
+                position: 'relative'
               }}
             >
-
+              <Typography 
+                sx={{
+                  color: '#000',
+                  fontSize: '28px',
+                  fontWeight: 700,
+                  lineHeight: '100%',
+                }}
+              >
+                Will count the price of your future banya
+              </Typography>
+              <Box
+                sx={{
+                  margin: '20px 0',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '8px',
+                  '&>*': {
+                    paddingLeft: '20px',
+                    position: 'relative',
+                    '&:before': {
+                      content: "' '",
+                      position: 'absolute',
+                      width: '8px',
+                      height: '8px',
+                      left: '0',
+                      top: 'calc(50% - 4px)',
+                      background: '#275F6C',
+                      borderRadius: '50%',
+                    }
+                  }
+                }}
+              >
+                <Typography>advantage number 1</Typography>
+                <Typography>advantage number 2. This is long advantage. Really very long. I need it to check behaviour on resize. this way i wouldn't forget lol\)</Typography>
+                <Typography>advantage number 3</Typography>
+              </Box>
+              <Box>
+                <TextField
+                  placeholder="8 900 800 70 60"
+                  variant="outlined"
+                  fullWidth
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Image
+                          priority
+                          src="/assets/svg/phone_icon.svg"
+                          height={24}
+                          width={24}
+                          alt="Follow us on Twitter"
+                        />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Box>
             </Box>
           </Grid>
         </Grid>
@@ -66,14 +130,3 @@ const FirstScreen = () => {
 }
 
 export default FirstScreen
-{/**
-color: #000;
-  font-size: 40px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 120%;
-  width: 620px;
-  max-width: 100%;
-  margin-bottom: 24px;
-}
-*/}
