1ad3a7f1193ec9e2ac4fa6b12d022cfb60c8fe4e
hunter <n.ohotnikov@collabox.dev>
2023-10-26 17:24:59 +0300

    bck-1_1: stsrted adding controllers

--- backend/database.sql
+++ backend/database.sql
@@ -1,13 +1,13 @@
-create Table person(
+create TABLE person(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   surname VARCHAR(255)
 );
 
-create Table post(
+create TABLE post(
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   content VARCHAR(255),
-  uesr_id INTEGER,
+  user_id INTEGER,
   FOREIGN KEY (user_id) REFERENCES person (id)
 );
